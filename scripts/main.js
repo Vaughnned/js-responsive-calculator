1a477d1fc06e93c51392621c78849f5594259460
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-05 14:19:56 -0400

    added gitignore

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,60 @@
-// alert("Hello!");
+(function () {
+  "use strict";
 
-function pushNumber(numberButtons) {
-  alert("Number");
-  document.querySelector.addEventListener("click", numberButtons);
-}
+  // alert("Hello!");
 
-function pushOperator() {
-  alert("Operator");
-  document.querySelector.addEventListener("click", operatorButtons);
-}
-
-let calculation = [];
-for (i = 0; i < calculation.length; i++) {
-  total += calculation[i];
-}
-
-let total = 0;
-
-function calculate() {
-  alert("Calculate");
-  document.querySelector.addEventListener("click", equalsButton);
-  let value = 0;
-  if (pushNumber() && pushOperator()) {
-    calculation.push(value);
+  function pushNumber(event) {
+    alert(event.target.value);
   }
 
-  //   if (example.classList.contains('class')) {}
-
-  //   for (i = 0; i < 1; i++) {
-  //     alert("");
-  //     return pushNumber(), calculation;
-  //   }
-}
-
-function enterNumber() {
   const numberButtons = document.querySelectorAll(".number");
   for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener("click", pushNumber);
   }
-}
 
-enterNumber();
-
-function enterOperator() {
-  const operatorButtons = document.querySelectorAll(".operator, .clear");
-  for (let i = 0; i < operatorButtons.length; i++) {
-    operatorButtons[i].addEventListener("click", pushOperator);
+  function pushOperator() {
+    alert("Operator");
+    document.querySelector.addEventListener("click", operatorButtons);
   }
-}
 
-enterOperator();
+  function calculate() {
+    alert("Calculate");
+    document.querySelector.addEventListener("click", equalsButton);
+    let total = 0;
+    let value = 0;
+    let calculation = [];
+    if (pushNumber() && pushOperator()) {
+      calculation.push(value);
+    }
+    if (operatorButtons.classList.contains(""))
+      for (i = 0; i < calculation.length; i++) {
+        total += calculation[i];
+      }
 
-function startCalculation() {
-  const equalsButton = document.querySelectorAll(".equal-sign");
-  for (let i = 0; i < equalsButton.length; i++) {
-    equalsButton[i].addEventListener("click", calculate);
+    //   if (example.classList.contains('class')) {}
   }
-}
 
-startCalculation();
+  // function enterNumber() {
+
+  // }
+
+  enterNumber();
+
+  function enterOperator() {
+    const operatorButtons = document.querySelectorAll(".operator, .clear");
+    for (let i = 0; i < operatorButtons.length; i++) {
+      operatorButtons[i].addEventListener("click", pushOperator);
+    }
+  }
+
+  enterOperator();
+
+  function startCalculation() {
+    const equalsButton = document.querySelectorAll(".equal-sign");
+    for (let i = 0; i < equalsButton.length; i++) {
+      equalsButton[i].addEventListener("click", calculate);
+    }
+  }
+
+  startCalculation();
+})();
